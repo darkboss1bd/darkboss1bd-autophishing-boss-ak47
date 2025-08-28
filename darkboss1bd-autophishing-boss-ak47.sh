9892ca1f6e1ef61a3c73883b68ad652e62a1885b
darkboss1bd <74151069+darkboss1bd@users.noreply.github.com>
2025-08-28 20:48:16 +0600

    darkboss1bd-autophishing-boss-ak47.sh

--- darkboss1bd-autophishing-boss-ak47.sh
+++ darkboss1bd-autophishing-boss-ak47.sh
@@ -13,7 +13,7 @@
 
 
 
-__version__="2.0"
+__version__="3.1"
 
 ## ANSI colors (FG & BG)
 RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
